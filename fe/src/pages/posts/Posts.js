de5ed8269619a8d4ac8458ca4fc87075470e85cf
Jason Kiraly <jasonkiraly@Jasons-MacBook-Pro.local>
2019-12-19 11:10:52 -0800

    formatted.

--- fe/src/pages/posts/Posts.js
+++ fe/src/pages/posts/Posts.js
@@ -1,7 +1,67 @@
 import React from "react";
+import { useQuery } from "@apollo/react-hooks";
+import { gql } from "apollo-boost";
+import { Spin, Alert, List, Typography } from "antd";
+
+import "./Posts.scss";
+
+const { Item } = List;
+const { Text } = Typography;
+
+const query = gql`
+  {
+    posts {
+      id
+      content
+      user {
+        id
+        firstName
+      }
+    }
+  }
+`;
 
 const Posts = () => {
-  return <div>Posts route</div>;
+  const { loading, error, data } = useQuery(query);
+  if (loading) return <Spin size="large" />;
+  if (error)
+    return (
+      <Alert
+        message={`Error with users fetch: ${error.message}`}
+        type="error"
+      />
+    );
+
+  console.log("data", data);
+
+  return (
+    <div>
+      <List
+        header={<div>Posts</div>}
+        bordered
+        dataSource={data.posts}
+        renderItem={({ content, user }) => {
+          return (
+            <Item className="list-item">
+              <Text mark>{content}</Text>
+              <Text className="user-span">User: {user.id}</Text>
+            </Item>
+          );
+        }}
+      />
+    </div>
+  );
 };
+
+// {
+//   data.posts.map(({ id, content, user }) => {
+//     return (
+//       <li className="list-item" key={id}>
+//         {content}
+//         <span className="user-span">User: {user.id}</span>
+//       </li>
+//     );
+//   });
+// }
 
 export default Posts;
--- fe/src/pages/users/Users.js
+++ fe/src/pages/users/Users.js
@@ -1,49 +1,7 @@
 import React from "react";
-import { useQuery } from "@apollo/react-hooks";
-import { gql } from "apollo-boost";
-import { Spin, Alert } from "antd";
-
-import "./Users.scss";
-const query = gql`
-  {
-    posts {
-      id
-      content
-      user {
-        id
-        firstName
-      }
-    }
-  }
-`;
 
 const Users = () => {
-  const { loading, error, data } = useQuery(query);
-  if (loading) return <Spin size="large" />;
-  if (error)
-    return (
-      <Alert
-        message={`Error with users fetch: ${error.message}`}
-        type="error"
-      />
-    );
-
-  console.log("data", data);
-
-  return (
-    <div>
-      <ul className="list">
-        {data.posts.map(({ id, content, user }) => {
-          return (
-            <li className="list-item" key={id}>
-              <span className="user-span">{user.firstName}:</span>
-              {content}
-            </li>
-          );
-        })}
-      </ul>
-    </div>
-  );
+  return <div>User route</div>;
 };
 
 export default Users;
