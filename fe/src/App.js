502007634b2871e757f1e924399d28db133925aa
Jason Kiraly <jasonkiraly@Jasons-MacBook-Pro.local>
2019-12-23 15:19:14 -0800

    Config with more open options

--- fe/src/App.js
+++ fe/src/App.js
@@ -2,26 +2,18 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import { ApolloProvider } from "@apollo/react-hooks";
-import { InMemoryCache } from "apollo-cache-inmemory";
-import { HttpLink } from "apollo-link-http";
 import { split } from "apollo-link";
+import { HttpLink } from "apollo-link-http";
+import { ApolloClient } from "apollo-client";
 import { WebSocketLink } from "apollo-link-ws";
 import { getMainDefinition } from "apollo-utilities";
-
-import ApolloClient from "apollo-boost";
+import { InMemoryCache } from "apollo-cache-inmemory";
 
 // Components
 import Routes from "./Routes";
 
 // Styles
 import "./App.css";
-
-const wsLink = new WebSocketLink({
-  uri: `ws://localhost:4000/graphql`,
-  options: {
-    reconnect: true
-  }
-});
 
 // What this does is track ALL your data by a unique id. You inform Apollo what will uniquely identify each data point
 // In our case, each record has a unique id property.
@@ -33,6 +25,13 @@ const cache = new InMemoryCache({
 
 const httpLink = new HttpLink({
   uri: "http://localhost:4000/graphql"
+});
+
+const wsLink = new WebSocketLink({
+  uri: `ws://localhost:4000/graphql`,
+  options: {
+    reconnect: true
+  }
 });
 
 const link = split(
